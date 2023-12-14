d8154a55a17b0775b0bbe58f1d3b56cb42e448ec
Aleksandra Cymer <aleksandra.cymer@onet.pl>
2023-12-14 19:56:20 +0100

    Publishing message for ur3

--- ur3e_aruco_controller/robot_node.py
+++ ur3e_aruco_controller/robot_node.py
@@ -2,7 +2,8 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Bool
-from geometry_msgs.msg import Point
+from builtin_interfaces.msg import Duration
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from cv_bridge import CvBridge
 import numpy as np
 import cv2
@@ -13,7 +14,15 @@ class RobotNode(Node):
 
         self.subscriber = self.create_subscription(Bool, "ArUcoCenterAboveHalf", self.subscription_callback, 10)
         self.prev_message = False # Previous message - change position only on posedge/negedge
+        self.jointPublisher = self.create_publisher(JointTrajectory, "scaled_joint_trajectory_controller/joint_trajectory", 10)
 
-
-    def subscription_callback(self):
-        pass
+    def subscription_callback(self, msg):
+        if msg.value != self.prev_message: 
+            trajectory = JointTrajectory(
+            joint_names=['elbow_joint'],
+            points=[
+                JointTrajectoryPoint(positions=0, time_from_start=Duration(sec=6, nanosec=0))
+                ],
+            )
+            self.jointPublisher.publish(trajectory)
+            self.prev_message = msg.value
